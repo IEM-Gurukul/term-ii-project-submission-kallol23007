9c54441c2ff09e289a0ebe4361706221b324651e
Kallolsen <kshetramohansen25@gmail.com>
2026-03-27 22:49:00 +0530

    Update MatchMaking.java

--- src/MatchMaking.java
+++ src/MatchMaking.java
@@ -3,15 +3,18 @@ import java.util.*;
 class MatchmakingSystem {
     private Queue<Player> queue = new LinkedList<>();
 
+   //method to add player to matchmaking queue
     public void joinQueue(Player p) throws Exception {
+        // Check if player already exists in queue
         if (queue.contains(p)) {
             throw new Exception("Player already in queue");
         }
         queue.add(p);
-        System.out.println(p.getName() + " joined matchmaking queue");
+        System.out.println("player " + p.getName() + "successfully joined matchmaking queue");
     }
 
     public GameRoom matchPlayers() {
+        System.out.println("Matching players...");
         if (queue.size() >= 2) {
             Player p1 = queue.poll();
             Player p2 = queue.poll();
