72e1beb89a61512aeeae78ea6b553e88d2f55785
Kallolsen <kshetramohansen25@gmail.com>
2026-03-29 18:27:52 +0530

    Refactor matchmaking thread logic and add stop method

--- src/MatchMakingThread.java
+++ src/MatchMakingThread.java
@@ -1,24 +1,38 @@
+// Thread responsible for continuously matching players
 class MatchmakingThread extends Thread {
+
     private MatchmakingSystem system;
+    private boolean running = true;
 
     public MatchmakingThread(MatchmakingSystem system) {
         this.system = system;
     }
 
+    public void stopThread() {
+        running = false;
+    }
+
+    private void processMatchmaking() {
+        GameRoom room = system.matchPlayers();
+        if (room != null) {
+            System.out.println("Match found! Starting game...");
+            room.startGame();
+        }
+    }
+
     public void run() {
-        while (true) {
-            System.out.println("Thread running... ");
+        while (running) {
+            System.out.println("Matchmaking thread is active...");
 
+            processMatchmaking();
 
-            GameRoom room = system.matchPlayers();
-            if (room != null) {
-                room.startGame();
-            }
             try {
                 Thread.sleep(2000);
             } catch (Exception e) {
-                System.out.println("Thread error");
+                System.out.println("Thread interrupted: " + e.getMessage());
             }
         }
     }
+
+    // TODO: Add matchmaking based on player skill level
 }
